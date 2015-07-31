221fd39012a119fd9c811a00934e6455d954c250
tom--bo <otsuka.t.2013@gmail.com>
2015-07-31 23:03:58 +0900

    ex2。テスト通らん

--- perl/lib/Parser.pm
+++ perl/lib/Parser.pm
@@ -2,12 +2,32 @@ package Parser;
 use strict;
 use warnings;
 
+use Log;
+
 sub new {
     my ($class, %args) = @_;
     return bless \%args, $class;
 }
 
 sub parse {
+    my $self = shift;
+    my $fileName = $self->{filename};
+    my $File = open LTSV, $fileName;
+    if ( !$File ) {
+        die "miss!: $!";
+    }
+    my $ret = [];
+    while(<LTSV>){
+        chomp;
+        my @tmp = split(/\t+/, $_);
+        my %obj;
+        foreach my $val (@tmp){
+            $val =~ /([a-z]*):(.*)/;
+            $obj{$1} = $2 unless $2 eq '-';
+        }
+        push($ret, Log->new(%obj));
+    }
+    return $ret;
 }
 
 1;
