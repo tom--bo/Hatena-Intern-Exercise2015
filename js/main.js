f96a12c3f8e3b72b41c8c51bfcd8f3e32e033a8a
tom--bo <otsuka.t.2013@gmail.com>
2015-08-02 07:22:19 +0900

    js ex2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
-var parseLTSVLog= function (LTSV) {
+var parseLTSVLog = function (LTSV) {
     var ret = [];
     var lines = LTSV.split("\n");
     var l = lines.length - 1;
@@ -23,4 +23,21 @@ var parseLTSVLog= function (LTSV) {
 }
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください
+var createLogTable = function (target, LTSV_arr) {
+
+    var table = document.createElement('table');
+    target.appendChild(table);
+
+    var thead = document.createElement('thead')
+    thead.innerHTML = '<tr><th>path</th><th>epoch</th></tr>';
+    table.appendChild(thead);
+
+    var tbody = document.createElement('tbody')
+    var len = LTSV_arr.length;
+    for(var i=0; i< len; i++){
+        tbody.innerHTML += "<tr><td>" + LTSV_arr[i]["path"] + "</td><td>" + LTSV_arr[i]["epoch"] + "</td></tr>";
+    }
+    table.appendChild(tbody);
+
+}
 
