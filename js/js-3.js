2ca4b81a4f1c7f96348a550bd3ac66d3ed23946c
tom--bo <otsuka.t.2013@gmail.com>
2015-08-02 08:11:11 +0900

    ボタンをトグルっぽく変更

--- js/js-3.js
+++ js/js-3.js
@@ -1,14 +1,25 @@
 // 課題 JS-3 の実装をここに記述してください。
 
-var tableBtnPressed = function() {
-    var target = document.getElementById("table-container");
-    var txt = document.getElementById("log-input").value;
+function btnClosure(first) {
+    var cnt = first;
 
-    var log = parseLTSVLog(txt);
-    console.log(target);
-    createLogTable(target, log); 
+    return function() {
+        var target = document.getElementById("table-container");
+
+        if(cnt%2 == 0) {
+            var txt = document.getElementById("log-input").value;
+
+            var log = parseLTSVLog(txt);
+            createLogTable(target, log); 
+        } else {
+            target.removeChild(target.firstChild);
+        }
+
+        return ++cnt;
+    }
 }
 
+var tableBtnPressed = btnClosure(0);
 var btn = document.getElementById("submit-button");
 
 btn.addEventListener("click", tableBtnPressed , false);
