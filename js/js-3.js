ed8f788d4fe6d1f6240b3325b2b1f8f2d231022c
tom--bo <otsuka.t.2013@gmail.com>
2015-08-02 07:49:10 +0900

    js ex3

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,16 @@
 // 課題 JS-3 の実装をここに記述してください。
+
+var tableBtnPressed = function() {
+    var target = document.getElementById("table-container");
+    var txt = document.getElementById("log-input").value;
+
+    var log = parseLTSVLog(txt);
+    console.log(target);
+    createLogTable(target, log); 
+}
+
+var btn = document.getElementById("submit-button");
+
+btn.addEventListener("click", tableBtnPressed , false);
+
+
