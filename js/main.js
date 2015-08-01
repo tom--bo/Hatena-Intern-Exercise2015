6cd2b2ceb1e39eb90d29c499691cf189a8ed98f3
tom--bo <otsuka.t.2013@gmail.com>
2015-08-02 06:20:31 +0900

    js ex1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
+var parseLTSVLog= function (LTSV) {
+    var ret = [];
+    var lines = LTSV.split("\n");
+    var l = lines.length - 1;
+
+    for(var i=0; i<l; i++){
+        var line_hash = {};
+        var val = lines[i].split("\t");
+        var v = val.length;
+        for(var j=0; j<v; j++){
+            if(val[j].indexOf("epoch:") != -1){
+                line_hash["epoch"] = Number(val[j].replace("epoch:", ""));
+            }
+            else if(val[j].indexOf("path:") != -1){
+                line_hash["path"] = val[j].replace("path:", "");
+            }
+        }
+        ret.push(line_hash);
+    }
+
+    return ret;
+}
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください
+
