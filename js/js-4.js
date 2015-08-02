1802f6c56ca37e1cc761d331b8f90e3fbead7aca
tom--bo <otsuka.t.2013@gmail.com>
2015-08-02 16:54:03 +0900

    prepare ex4

---
+++ js/js-4.js
@@ -0,0 +1,11 @@
+// 課題 JS-4
+
+var searchBtnPressed =  function() {
+    console.log("hello?seach");
+}
+
+var searchBtn = document.getElementById("search-container");
+
+btn.addEventListener("click", searchBtnPressed , false);
+
+
